30bbbd857269e5bedb009e6bee3b406d5864afd5
sayalidhond9898-cmyk <sayalidhond9898@gmail.com>
2025-12-04 12:35:59 +0530

    Add files via upload

---
+++ Python/Activities/activity1.py
@@ -0,0 +1,11 @@
+name = input("What is your name? ")
+	
+age = int( input( "How old are you: " ) )
+year = 2024 +(100 - age)
+print( name + " will be 100 years old in the year " + str(year) )
+
+
+""" txt = "The rain in Spain stays mainly in the plain"
+
+print(txt.replace("Spain", "India"));
+print(txt.replace("in", "on"));"""
