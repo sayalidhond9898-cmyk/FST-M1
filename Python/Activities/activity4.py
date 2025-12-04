df6be8abe99fb752890af1c1033a9d1f7812c732
sayalidhond9898-cmyk <sayalidhond9898@gmail.com>
2025-12-04 12:51:05 +0530

    Add files via upload

---
+++ Python/Activities/activity4.py
@@ -0,0 +1,33 @@
+# Two-player Rock-Paper-Scissors game with replay option
+
+def rps_game():
+    print("Rock-Paper-Scissors Game")
+    print("------------------------")
+
+    while True:
+        # Player inputs
+        player1 = input("Player 1, enter your choice (rock/paper/scissors): ").lower()
+        player2 = input("Player 2, enter your choice (rock/paper/scissors): ").lower()
+
+        # Rules logic
+        if player1 == player2:
+            print("It's a tie!")
+        elif (player1 == "rock" and player2 == "scissors") or \
+             (player1 == "scissors" and player2 == "paper") or \
+             (player1 == "paper" and player2 == "rock"):
+            print("Player 1 wins!")
+        elif (player2 == "rock" and player1 == "scissors") or \
+             (player2 == "scissors" and player1 == "paper") or \
+             (player2 == "paper" and player1 == "rock"):
+            print("Player 2 wins!")
+        else:
+            print("Invalid input! Please choose rock, paper, or scissors.")
+
+        # Replay option
+        play_again = input("Do you want to play another round? (yes/no): ").lower()
+        if play_again != "yes":
+            print("Thanks for playing! Goodbye.")
+            break
+
+# Run the game
+rps_game()
