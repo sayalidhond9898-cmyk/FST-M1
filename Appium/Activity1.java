4da0d1170a14efbb303e942719bbab0e914cf8f9
sayalidhond9898-cmyk <sayalidhond9898@gmail.com>
2026-02-13 10:56:00 +0530

    three different locators

--- Appium/Activity1.java
+++ Appium/Activity1.java
@@ -1,14 +1,10 @@
 package mobileexample;
 
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.time.Duration;
-import java.util.List;
 
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.io.File;
+import java.net.URI;
+import java.net.URL;
+
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -16,53 +12,58 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 
 public class Activity1 {
-	
-	    WebDriverWait wait;
-	    AndroidDriver driver;
-
-	    // Set up method
-	    @BeforeClass
-	    public void setUp() throws MalformedURLException, URISyntaxException {
-	        // Desired Capabilities
-	    	File appFile = new File("src/test/resources/calculator.apk");
-	        UiAutomator2Options options = new UiAutomator2Options();
-	        options.setPlatformName("android");
-	        options.setAutomationName("UiAutomator2");
-	        options.setApp(appFile.getAbsolutePath());
-	        options.noReset();
-
-	        // Server URL
-	        URL serverURL = new URI("http://127.0.0.1:4723").toURL();
-
-	        // Driver initialization
-	        driver = new AndroidDriver(serverURL, options);
-	        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-	        // Open the page in browser
-	       // driver.get("https://www.training-support.net/selenium/target-practice");
-	    }
-
-	    @Test
-	    public void listElements() {
-	    	driver.findElement(AppiumBy.id("digit_8")).click();
-	    	driver.findElement(AppiumBy.accessibilityId("multiply")).click();
-	    	driver.findElement(AppiumBy.id("digit_6")).click();
-	    	
-	    	driver.findElement(AppiumBy.accessibilityId("equals")).click();
-	    	String result = driver.findElement(AppiumBy.id("result_final")).getText();
-	    	Assert.assertEquals(result, "48");
-	    	
-	        // Wait for elements to load
-	    }
-
-	    @AfterClass
-	    public void afterClass() {
-	        driver.quit();
-	    }
-	
-
+    
+    AppiumDriver driver;
+    WebDriverWait wait;
+    
+    @BeforeClass
+    public void setUp() throws Exception {
+        // Set the app file
+        File file = new File("src/main/resources/Calculator.apk");
+        
+        // Desired capabilities
+        UiAutomator2Options caps = new UiAutomator2Options();
+        caps.setPlatformName("android");
+        caps.setAutomationName("UiAutomator2");
+        caps.setApp(file.getAbsolutePath());
+        caps.noReset();
+        
+        // Set Appium server URL
+        URL serverURL = new URI("http://localhost:4723").toURL();
+        
+        // Initialize driver
+        driver = new AndroidDriver(serverURL, caps);
+    }
+    
+    @Test
+    public void testMethod() {
+        // Locator 1: ID
+        driver.findElement(AppiumBy.id("digit_8")).click();
+        
+        // Locator 2: Accessibility ID
+        driver.findElement(AppiumBy.accessibilityId("multiply")).click();
+        
+        // Locator 3: XPath
+        driver.findElement(AppiumBy.xpath("//android.widget.Button[@content-desc='5']")).click();
+        
+        // Again using Accessibility ID for equals
+        driver.findElement(AppiumBy.accessibilityId("equals")).click();
+        
+        // Fetch result using ID
+        String result = driver.findElement(AppiumBy.id("result_final")).getText();
+        
+        // Assertion
+        Assert.assertEquals(result, "40");
+    }
+    
+    @AfterClass
+    public void afterTest() {
+        driver.quit();
+    }
 }
+
